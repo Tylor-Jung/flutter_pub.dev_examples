6d0c61c62a0d7ff9b134e35e5988fd319caff191
tylor <info@ettecompany.com>
2022-08-03 22:28:48 +1000

    feat: image_picker package, example view

--- lib/page/HomePage.dart
+++ lib/page/HomePage.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:study_package/main.dart';
+import 'package:timezone/data/latest_all.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -65,11 +67,34 @@ class _HomePageState extends State<HomePage> {
               if (!permission) {
                 return;
               }
-              await flutterLocalNotificationsPlugin.show(
-                0,
-                'plain title',
-                'plain body',
+              // await flutterLocalNotificationsPlugin.show(
+              //   0,
+              //   'plain title',
+              //   'plain body',
+              //   detail,
+              // );
+
+              // 타임존 셋팅 필요
+              final now = tz.TZDateTime.now(tz.local);
+              var notiDay = now.day;
+
+              await notification.zonedSchedule(
+                1,
+                'alarmTitle',
+                'alarmDescription',
+                tz.TZDateTime(
+                  tz.local,
+                  now.year,
+                  now.month,
+                  now.day,
+                  now.hour,
+                  now.minute + 1,
+                ),
                 detail,
+                androidAllowWhileIdle: true,
+                uiLocalNotificationDateInterpretation:
+                    UILocalNotificationDateInterpretation.absoluteTime,
+                matchDateTimeComponents: DateTimeComponents.time,
               );
             },
             child: const Text('add alram'),
